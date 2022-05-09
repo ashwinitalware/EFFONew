089df063d5648eaac9e0d4faecdf104f37ff1fa2
ashwinitalware <ashwinitalware321@gmail.com>
2022-05-09 11:04:30 +0530

    ride page

--- src/app/Cabservice/ridestatus/ridestatus.page.ts
+++ src/app/Cabservice/ridestatus/ridestatus.page.ts
@@ -6,8 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ridestatus.page.scss'],
 })
 export class RidestatusPage implements OnInit {
-
-  constructor() { }
+  height = 0
+  constructor() {
+    this.height = window.innerHeight
+  }
 
   ngOnInit() {
   }
